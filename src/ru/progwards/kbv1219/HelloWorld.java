a8854d93ddb95e0829d8ee1668af18d7b0276423
Boris <abirme@yandex.ru>
2019-12-11 20:23:17 +0300

    My first 'commit'

---
+++ src/ru/progwards/kbv1219/HelloWorld.java
@@ -0,0 +1,39 @@
+package ru.progwards.kbv1219;
+
+public class HelloWorld {
+    static void helloWorld(){
+        String str ;
+        str = "Привет, мир!";
+        System.out.println(str);
+        System.out.println("Hello World!");
+    }
+    static void helloProgwards(){
+        System.out.println("Привет Progwards!");
+    }
+
+    static int addition (int x,int y) {
+        int z = x+y;
+        return z;
+    }
+    static int addition2 (int x,int y){
+        return x+y;
+    }
+
+    static void println(String str) {
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        helloWorld();
+        helloProgwards();
+
+        HelloWorld.helloWorld();  //вызов из класса
+
+        ru.progwards.kbv1219.HelloWorld.helloWorld();
+
+        println("Привет!");
+        println("Приветствую!");
+        int z = addition(2,3);
+        System.out.println(z);
+        System.out.println(addition2(3,3));
+    }
+}
