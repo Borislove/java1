b04b24e62c2bab1cd85333f37a8c9c02921c2479
Boris <abirme@yandex.ru>
2019-12-12 19:39:17 +0300

    Astronomy (решаем)

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -1,5 +1,4 @@
 package ru.progwards.java1.lessons.basics;
-
 //В этой задаче нужно будет реализовать функции, вычисляющих разные астрономические величины.
 // Во всех функциях, где это необходимо, считаем число π = 3.14
 public class Astronomy {
@@ -9,41 +8,76 @@ public class Astronomy {
 
     //вычисляет площадь поверхности сферы радиуса R по формуле S = 4πR2
     public static Double sphereSquare(Double r) {
-
-        double S= 4*pi*r*2;
-        System.out.println(S);
+        double S= 4*pi*r*r;
+     //   System.out.println(S);
         return S;
     }
-    //площадь поверхности Земли, считая радиус равным 6 371.2 км и используют функцию sphereSquare
-    public static Double earthSquare() {
-
-        final Double er = 6371.2;
-        System.out.println(er);
-
-        return er;
+    public static Double earthSquare(){
+        double earth = 6371.2;
+        sphereSquare(earth);
+       System.out.println("Площадь поверхности Земли = "+ sphereSquare(earth));
+     //  long le = sphereSquare(earth).longValue();
+     //   System.out.println(le);
+        return sphereSquare(earth);
     }
 
-    //лощадь поверхности Меркурия, считая радиус равным 2 439,7 км  и используют функцию sphereSquare
-    public static Double mercurySquare() {
-        final Double er = 2439.7;
-        System.out.println(er);
-        return er;
+    public static Double mercurySquare(){
+        double mercury = 2439.7;
+      sphereSquare(mercury);
+       System.out.println("Площадь поверхности Меркурия = "+ sphereSquare(mercury));
+     // long le = sphereSquare(mercury).longValue();
+    //  System.out.println(le);
+        return null;
     }
-//площадь поверхности Юпитера, считая радиус равным 71 492 км  и используют функцию sphereSquare
+
     public static Double jupiterSquare(){
-        final Double er = 71492.0;
-        System.out.println(er);
-        return er;
-
+        double jupiter = 71492;
+        sphereSquare(jupiter);
+       System.out.println("Площадь поверхности Юпитера = "+ sphereSquare(jupiter));
+   //    long le = sphereSquare(jupiter).longValue();
+   //    System.out.println(le);
+        return sphereSquare(jupiter);
     }
 
-    //public static Double earthVsMercury(), которая вычисляет отношение площади поверхности Земли к площади поверхности Меркурия используя готовые функции площадей планет
+    public static Double earthVsMercury(){
+
+     //   double vs =  mercurySquare()*2 ;
+   //   double earthVsMercury = earthSquare() / mercurySquare();
+        double earthVsMercury = mercurySquare() / earthSquare();
+        //long le = earthVsMercury(earthSquare()).longValue() / e;
+
+       // long les = earthSquare().longValue() / mercurySquare().longValue();
+      System.out.println("отношение площади поверхности Земли к площади поверхности Меркурия = "+ earthVsMercury);
+        //Long le = (earthSquare()) / (mercurySquare());
+    //    return earthVsMercury;
+
+        return earthVsMercury;
+    }
+
+    /*public static Double earthVsJupiter(){
+        double earthVsJupiter = (earthSquare()/jupiterSquare());
+        System.out.println("отношение площади поверхности Земли к площади поверхности Юпитера = " +earthVsJupiter);
+        return null;
+    } */
+
+
+    public static Double earthVsJupiter(){
+
+    /*double ear = jupiterSquare().doubleValue() /earthSquare().doubleValue() ;
+    long ears = jupiterSquare().longValue() / earthSquare().longValue();
+        System.out.println(ear);
+    */return null;
+    }
+
     public static void main(String[] args) {
 
-    sphereSquare(5.0);
-    sphereSquare(earthSquare());
-    sphereSquare(mercurySquare());
-    sphereSquare(jupiterSquare());
+    //sphereSquare(2.0);
 
+earthSquare();
+mercurySquare();
+jupiterSquare();
+
+//earthVsMercury();
+//earthVsJupiter();
     }
 }
