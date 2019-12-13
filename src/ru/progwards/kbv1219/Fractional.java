9833af01d12492eb7fe32cf23723004ff804dc6b
Boris <abirme@yandex.ru>
2019-12-14 00:00:35 +0300

    Fractional - в 1 строчку

--- src/ru/progwards/kbv1219/Fractional.java
+++ src/ru/progwards/kbv1219/Fractional.java
@@ -4,19 +4,35 @@ package ru.progwards.kbv1219;
 * Напишите метод, который возвращает дробную часть числа. Сигнатура метода static double fractional(double num)
 
 Например fractional(1.53) должен выдать 0.53
-* */
+ */
 public class Fractional {
 
     //static double num;
     static double fractional(double num) {
 
-     Integer value = new Integer((int) num);
+     //Integer value = new Integer((int) num);
+
+
+     /*   int value = (int)num;  //сократил
      double number = num - value;
        System.out.println(number);
-       return number;
+       return number; */
+
+
+        //System.out.println(num);
+       //return new Integer((int)num)  ;
+
+        //вариант 2
+        /*
+         num = num - (int)num %(100) ;
+        System.out.println(num);
+        return num ; */
+
+        //упрощенный вариант 2
+        return num - num %(100) ;
     }
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 
         fractional(0.0);
         fractional(1.0);
