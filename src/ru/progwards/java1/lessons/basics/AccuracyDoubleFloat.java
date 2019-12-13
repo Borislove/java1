2b7f103791a4113d823bc7295205faea3a710e80
Boris <abirme@yandex.ru>
2019-12-13 16:51:35 +0300

    AcuracyDoubleFloat (1)

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -1,28 +1,42 @@
 package ru.progwards.java1.lessons.basics;
 
+//что то намудрил...валидатор проверяй!
 public class AccuracyDoubleFloat {
 
-       /* public static Double sVolume(Double r) {
+    // формула объема шара  V = 4/3 pi * R* R* R
+    final static Double pi = 3.14;
+    final static Double R = 6371.2;
 
-        }
-        public static Float sVolume(Double r) {
-        }
 
         public static double volumeBallDouble(double radius){
+          double V = 4/3 * pi * R * R *R ;
+          //  System.out.println(V);
+            return V;
         }
 
         public static float volumeBallFloat(float radius){
+            float V = (float)(4/3 * pi * R * R *R);
+          //  System.out.println(V);
+            return V;
 
         }
-
 
         public static double calculateAccuracy(double radius){
-            return 0;
+           double razn = (double) (volumeBallDouble(R)) / (volumeBallFloat(R.floatValue()));
+           razn = 4/3 * pi * razn * razn * razn ;
+
+           //double a = volumeBallFloat(V);
+           System.out.println(razn);
+            return razn;
         }
-*/        public static void main(String[] args) {
+        public static void main(String[] args) {
 
            // sVolume();
-           // volumeBallDouble();
-           // volumeBallFloat();
+          //volumeBallDouble(6371.2);
+          //volumeBallFloat(R.floatValue());
+         //   volumeBallFloat(R.floatValue());
+            //calculateAccuracy(volumeBallDouble(R) / volumeBallFloat(R.floatValue()));
     }
+
+
 }
