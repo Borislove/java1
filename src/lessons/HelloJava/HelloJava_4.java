afc60f0f36c5a2e06c5fed3087a2b03bb96b5cd8
Boris <abirme@yandex.ru>
2019-12-15 18:27:21 +0300

    Hello Java! v4

---
+++ src/lessons/HelloJava/HelloJava_4.java
@@ -0,0 +1,9 @@
+package lessons.HelloJava;
+
+import static java.lang.System.out;
+
+public class HelloJava_4 {
+    public static void main(String[] args) {
+        out.println ("Hello Java! v4");
+    }
+}
