2d057dc6c8786df90c62c600e24ca36daa1ca77f
Boris <abirme@yandex.ru>
2019-12-15 18:21:04 +0300

    Hello Java! v2

---
+++ src/lessons/HelloJava/HelloJava_2.java
@@ -0,0 +1,8 @@
+package lessons.HelloJava;
+
+public class HelloJava_2 {
+    public static String h = "Hello Java! v2" ;
+    public static void main(String[] args) {
+        System.out.println(h);
+    }
+}
