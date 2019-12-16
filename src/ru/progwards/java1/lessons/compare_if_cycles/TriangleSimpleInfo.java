43ebd04e328ccceaaaa9d5ec50d631afbf4ab9a9
Boris <abirme@yandex.ru>
2019-12-16 19:33:04 +0300

    TriangleSimpleInfo (6)  - test 1 ok

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -36,10 +36,10 @@ public class TriangleSimpleInfo {
     public static int minSide(int a, int b, int c) {  //вернуть наименьшую сторону треугольника
 
 
-        if (a < b & a < c) {
+        if (a <= b & a <= c) {
             System.out.println(a);
           return a;
-        } else if (b < a & b < c) {
+        } else if (b <= a & b <= c) {
             System.out.println(b);
            return b;
         } else {
