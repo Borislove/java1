75817818c8507de4a3a58d3350bf62f02ccfb65d
Boris <abirme@yandex.ru>
2019-12-16 18:34:07 +0300

    TriangleSimpleInfo (2)

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -10,38 +10,64 @@ public static boolean isEquilateralTriangle(int a, int b, int c), которая
  */
 
 // TODO: 16.12.2019
+//делаю поэтапно, но пока не дается, тесты со сбоями
+//ERROR: Тест не был закончен. Возможной причиной является бесконечный цикл в коде, либо слишком долгое его выполнение. Исключение:  , догадываюсь
 
 public class TriangleSimpleInfo {
 
-    public static int a,b,c;
-    public static int maxSide(int a, int b,int c) {  //вернуть наибольшую сторону треугольника
+    public static int a, b, c;
 
+    public static int maxSide(int a, int b, int c) {  //вернуть наибольшую сторону треугольника
 
-        if (a>b & a>c) {
+//вроде работает
+        if (a > b & a > c) {
             System.out.println(a);
-            return a;}
-        else if (b>a & b>c) {
+            return a;
+        } else if (b > a & b > c) {
             System.out.println(b);
             return b;
-         }
-        else {
+        } else {
             System.out.println(c);
             return c;
         }
     }
 
-    public static int minSide(int a,int b,int c) {  //вернуть наименьшую сторону треугольника
-        return 0;
+    public static int minSide(int a, int b, int c) {  //вернуть наименьшую сторону треугольника
+
+
+        if (a < b & a < c) {
+            System.out.println(a);
+            return a;
+        } else if (b < a & b < c) {
+            System.out.println(b);
+            return b;
+        } else {
+            System.out.println(c);
+            return c;
+        }
+
     }
 
-    public static boolean isEquilateralTriangle(int a, int b ,int c){
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
 
+
+        return false;
         // TODO: 16.12.2019  да как же закрывать функции пустые...
-        return isEquilateralTriangle(a,b,c); //? вроде закрылось
+        //  return isEquilateralTriangle(a,b,c); //? вроде закрылось , с ошибкой походу....
+        //return 0;  -
+        //return null;
+
+
 
     }
+
+
+
+
+
 
     public static void main(String[] args) {
+
 
         maxSide(1,2,3);
         maxSide(1,3,2);
@@ -50,8 +76,11 @@ public class TriangleSimpleInfo {
         maxSide(3,1,2);
         maxSide(3,2,1);
 
-//        minSide(4,2,1);
-        //minSide(1,2,3);
-        //isEquilateralTriangle(1,2,3);
+        minSide(1, 2, 3);
+        minSide(1, 3, 2);
+        minSide(2, 1, 3);
+        minSide(2, 3, 1);
+        minSide(3, 1, 2);
+        minSide(3, 2, 1);
     }
 }
