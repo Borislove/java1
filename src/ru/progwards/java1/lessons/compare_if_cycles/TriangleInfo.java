b851301f987bf1f07fece0c4e186a47944c21320
Boris <abirme@yandex.ru>
2019-12-17 20:59:41 +0300

    TriangleInfo (15)  / test 3 - ok , test 2 - ok , test 1 = false!!!

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -1,35 +1,28 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
-/*
-* Задача 2. Класс TriangleInfo
-2.1 Реализовать функцию
-public static boolean isTriangle(int a, int b, int c), которая возвращает true, если по данным трём сторонам (a, b, c) можно построить треугольник.
-*  Из геометрии известно, что в треугольнике длина каждой из сторон меньше суммы длин двух других сторон.
-2.2 Реализовать функцию
-public static boolean isRightTriangle(int a, int b, int c), которая возвращает true, если треугольник со сторонами a, b, c является прямоугольным.
-*  Из геометрии известно, что для прямоугольного треугольника выполняется теорема Пифагора (сумма квадратов катетов равна квадрату гипотенузы).
-2.3 Реализовать функцию
-public static boolean isIsoscelesTriangle(int a, int b, int c), которая возвращает true, если треугольник со сторонами a, b, c является равнобедренным.
-*  Из геометрии известно, что в равнобедренном треугольнике есть две равные стороны.
-* */
-
-
 public class TriangleInfo {
 
      static int a,b,c;
 
     public static boolean isTriangle(int a, int b, int c) {
 
-        // TODO: 17.12.2019 С ЭТИМ методом что то не так
+        // TODO: 17.12.2019
 
+        //все ок
+         //if (a + b >= c & a > 0 & b > 0 & c > 0 || a + c >= b & a > 0 & b > 0 & c > 0 || b + c >= a & a > 0 & b > 0 & c > 0) {
+           // System.out.println("Можно построить треугольник! congratulation! " + "длина A = " + a + " длина B =  " + b + " длина C = " + c);
+            //return true;
 
-        //бред какойто!
-      if (a+b>=c &a>0 & b>0 & c>0) {
-          System.out.println("Строим!");
-           return false;
-        }
+        if (a+b>c || a+c>b || a+c>b & a>0 & b>0 &c>0) {
+
+            System.out.println("stroim");
         return true;
 
+
+    } else {
+            System.out.println("Треугольник построить нельзя!" +"А = " +a  + " B = " + b + " С  = "  + c );
+            return false;
+        }
     }
 
     public static boolean isRightTriangle(int a, int b, int c) {
@@ -41,7 +34,6 @@ public class TriangleInfo {
 //        System.out.println(" a^2 = " + Math.pow(a,e));
 //        System.out.println(" b^2 = " + Math.pow(b,e));
 //        System.out.println(" c^2 = " + Math.pow(c,e));
-
 
     //квадраты
         a = a * a;
@@ -61,7 +53,7 @@ public class TriangleInfo {
         //Из геометрии известно, что в равнобедренном треугольнике есть две равные стороны.
         //значит что то должно быть равно...логично
 
-if (a==b || b==c || a==c) {
+    if (a==b || b==c || a==c) {
         System.out.println("Какойто из треугольничков равнобедренный ММмм, а какой?"+" длина A = "+ a +" длина B =  "+b+" длина C = "+c);
         return true;
 
@@ -70,22 +62,22 @@ if (a==b || b==c || a==c) {
         return false;
 }
     }
-
-
-
     public static void main(String[] args) {
 
+        isTriangle(1,1,2);
         isTriangle(1,2,3);
         isTriangle(-3,2,-2);
-        isRightTriangle(1,2,3);
-        isRightTriangle(3,4,5); // сработало Оо
-        isRightTriangle(6,8,10);
+        isTriangle(2,-1,2);
+        isTriangle(-2,-2,-2);
+        isTriangle(0,0,0);
+        isTriangle(0,2,-3);
+     //   isRightTriangle(1,2,3);
+     //   isRightTriangle(3,4,5); // сработало Оо
+     //   isRightTriangle(6,8,10);
 
-        isIsoscelesTriangle(2,2,3);
-        isIsoscelesTriangle(2,3,2);
-        isIsoscelesTriangle(3,2,2);
-        isIsoscelesTriangle(1,2,5);  //ошибка
-
-
+       // isIsoscelesTriangle(2,2,3);
+       // isIsoscelesTriangle(2,3,2);
+      //  isIsoscelesTriangle(3,2,2);
+      //  isIsoscelesTriangle(1,2,5);  //ошибка
     }
 }
