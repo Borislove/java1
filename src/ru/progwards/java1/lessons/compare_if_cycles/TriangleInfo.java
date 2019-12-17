ca75e9bca2dc157eff2c46391b92960acc9c4ba5
Boris <abirme@yandex.ru>
2019-12-17 10:58:11 +0300

    TriangleInfo (12)  / test 3 - ok , test 2 - ok , test 1 = false!!!

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -22,15 +22,16 @@ public class TriangleInfo {
 
         // TODO: 17.12.2019 С ЭТИМ методом что то не так
 
-        if (a+b<c & a>0 &b>0 &c>0){
+        if (a+b<c){
              //|| a+c>=b & a>0 &b>0 &c>0|| b+c>=a & a>0 &b>0 &c>0) {
 
-                        System.out.println("Можно построить треугольник " +"длина A = "+ a +" длина B =  "+b+" длина C = "+c);
-            return true;
+            System.out.println("ТрЭугольника нельзя построить!!!а какой?" + a +""+b+""+c);
+
+            return false;
 
         } else {
-            System.out.println("ТрЭугольника нельзя построить!!!а какой?" + a +""+b+""+c);
-            return false;
+            System.out.println("Стройся уже!");
+            return true;
         }
 
     }
