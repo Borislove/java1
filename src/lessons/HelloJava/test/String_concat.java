f4ae606743d9fa91908b32aee06ec920a530a2fe
Boris <abirme@yandex.ru>
2019-12-19 02:38:34 +0300

    Bit ?

---
+++ src/lessons/HelloJava/test/String_concat.java
@@ -0,0 +1,15 @@
+package lessons.HelloJava.test;
+
+//N1.3 Первая программа
+public class String_concat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world)");
+        System.out.println(99);
+        System.out.println("7+1");
+        System.out.println(7+1);
+        System.out.println("7"+"1");
+        System.out.println("Кофе стоит "+ 100 + "руб");
+        System.out.println("Кофе " + 50+50 + " руб");
+    }
+}
