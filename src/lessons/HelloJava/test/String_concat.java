5d68d1754eee5491a987b3840a9190a1cb127262
Boris <abirme@yandex.ru>
2019-12-19 02:44:16 +0300

    Когда скучно...

--- src/lessons/HelloJava/test/String_concat.java
+++ src/lessons/HelloJava/test/String_concat.java
@@ -3,7 +3,8 @@ package lessons.HelloJava.test;
 //N1.3 Первая программа
 public class String_concat {
 
-    public static void main(String[] args) {
+    public static void thisTest(){
+
         System.out.println("Hello, world)");
         System.out.println(99);
         System.out.println("7+1");
@@ -11,5 +12,9 @@ public class String_concat {
         System.out.println("7"+"1");
         System.out.println("Кофе стоит "+ 100 + "руб");
         System.out.println("Кофе " + 50+50 + " руб");
+    }
+
+    public static void main(String[] args) {
+                thisTest();
     }
 }
