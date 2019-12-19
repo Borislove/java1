83a7fd06f9c56058bd995c8d975b161a827c6ece
Boris <abirme@yandex.ru>
2019-12-19 22:52:03 +0300

    Примеры объявления костант.

---
+++ src/ru/progwards/Main.java
@@ -0,0 +1,26 @@
+package ru.progwards;
+
+//N2.4 БНФ, константы, операторы, приоритеты
+//Примеры объявления констант
+
+public class Main{
+
+    public static final int DAYS_IN_WEEK = 7;
+    private static final int HOURS_IN_DAY = 24;
+    static final int MINUTES_IN_HOUR = 60;
+
+    //final int SECONDS_IN_MINUTE = 60;  //без статик ошибка будет
+    public static void main(String[] args) {
+        final int SECONDS_IN_MIN = 60;
+
+        System.out.print("Секунд в сутках: ");
+        System.out.println(MINUTES_IN_HOUR*HOURS_IN_DAY*SECONDS_IN_MIN);
+
+        System.out.print("Секунд в неделе: ");
+        System.out.println(MINUTES_IN_HOUR * HOURS_IN_DAY*DAYS_IN_WEEK*SECONDS_IN_MIN);
+
+        System.out.print("Секунд в часе: ");
+        System.out.println(SECONDS_IN_MIN* MINUTES_IN_HOUR);
+
+    }
+}
