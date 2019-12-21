e7d1a94a19dad69b7fc01291701f4f36e7d73b60
Boris <abirme@yandex.ru>
2019-12-21 05:25:00 +0300

    пример 2*2

---
+++ src/ru/progwards/T4/IfElse1.java
@@ -0,0 +1,20 @@
+package ru.progwards.T4;
+
+public class IfElse1 {
+    public static void main(String[] args) {
+        if(2*2 == 5)
+            System.out.println("Оказывается 2*2 = 5");
+
+        if (2*2 == 4)
+            System.out.println("Все таки 2*2 = 4");
+        else
+            System.out.println("Действительно, 2*2 не 4");
+
+        if(2*2 < 0)
+            System.out.println("2*2 это отрицательное число");
+        else if(2*2 >0)
+            System.out.println("2*2 это положительное число");
+        else
+            System.out.println("2*2 это и есть ноль");
+    }
+}
