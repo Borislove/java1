bf799bc6eba2e03fa7a5e9aca75937bcdd61b843
Boris <abirme@yandex.ru>
2019-12-24 12:43:44 +0300

    Похоже на прогрессию...

---
+++ src/lessons/HelloJava/test/Geometric.java
@@ -0,0 +1,16 @@
+package lessons.HelloJava.test;
+
+
+//Геометрическая прогрессия Oo , сама написалась...
+public class Geometric {
+    public static void main(String[] args) {
+
+        int a =1;
+
+        for (int i=1; i<15;i++ ){
+            a= a+a;
+            System.out.println(a);
+        }
+
+    }
+}
