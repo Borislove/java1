a9a3db49ba5dfe3c1c943241430721419cbbe1df
Boris <abirme@yandex.ru>
2019-12-26 14:53:09 +0300

    Array Examples (Примеры массивов)

---
+++ src/ru/progwards/T6/ArrayFunction.java
@@ -0,0 +1,13 @@
+package ru.progwards.T6;
+
+public class ArrayFunction {
+
+    private static void printArray(int[] intArray){
+
+    }
+    public static void main(String[] args) {
+
+        int[] simpleArray1 = {21,34,55,89,144};
+        printArray(simpleArray1);
+    }
+}
