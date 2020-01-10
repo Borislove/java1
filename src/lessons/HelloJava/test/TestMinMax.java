b5c48e2741c39ae87fa72f539b8e247326a65cbc
Boris <abirme@yandex.ru>
2020-01-10 08:11:14 +0300

    Небольшой штрих

--- src/lessons/HelloJava/test/TestMinMax.java
+++ src/lessons/HelloJava/test/TestMinMax.java
@@ -3,7 +3,6 @@ package lessons.HelloJava.test;
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
 
-
 //минимальные и максимальные значения
 public class TestMinMax {
 
@@ -11,23 +10,39 @@ public class TestMinMax {
     public static Integer aMax = MAX_VALUE;
     public static Integer aMin = MIN_VALUE;
 
-    public static Short s;
+
+    public static void Double(){
+        System.out.println("Double min "+Double.MIN_VALUE);
+        System.out.println("Double max "+Double.MAX_VALUE);
+    }
+
+    public static void Float(){
+        System.out.println("Float min "+Float.MIN_VALUE);
+        System.out.println("Float max "+Float.MAX_VALUE);
+    }
+
+    public static void Long(){
+        System.out.println("Long min "+Long.MIN_VALUE);
+        System.out.println("Long max "+Long.MAX_VALUE);
+    }
+
+    public static void Short(){
+        System.out.println("Short min "+ Short.MIN_VALUE);
+        System.out.println("Short max "+ Short.MAX_VALUE);   // нужно использовать класс обёртку
+        System.out.println(Short.MIN_VALUE+Short.MAX_VALUE);
+    }
+
+    public static void Integer() {
+        System.out.println("Integer min " + aMin);
+        System.out.println("Integer max  " + aMax);
+        //System.out.println(aMin+aMax); // -1
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Integer min " + aMin);
-        System.out.println("Integer max  " + aMax);
-
-        System.out.println("Short min "+ Short.MIN_VALUE);
-        System.out.println("Short max "+ Short.MAX_VALUE);   // нужно использовать класс обёртку
-
-        System.out.println("Long min "+Long.MIN_VALUE);
-        System.out.println("Long max "+Long.MAX_VALUE);
-
-        System.out.println("Double min "+Double.MIN_VALUE);
-        System.out.println("Double max "+Double.MAX_VALUE);
-
-        System.out.println("Float min "+Float.MIN_VALUE);
-        System.out.println("Float max "+Float.MAX_VALUE);
+        //Integer();
+        //Short();
+        //Float();
+        //Double();
     }
 }
