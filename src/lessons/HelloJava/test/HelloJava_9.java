3ea68408152153832b33c5e313888e4e472fa651
Boris <abirme@yandex.ru>
2020-01-10 19:42:47 +0300

    hello hash ...

---
+++ src/lessons/HelloJava/test/HelloJava_9.java
@@ -0,0 +1,33 @@
+package lessons.HelloJava.test;
+
+public class HelloJava_9 {
+
+
+
+    public static void main(String[] args) {
+
+     //   Character a = 'V';
+
+        String code = "72"; //
+        String code101 = "101"; //101
+
+        String code108 = "108";//108 x2
+        String code111 = "111";//111
+
+        String code74 = "74";// 74
+        String code65 = "65";//65
+        String code86 = "86";//86
+
+       // System.out.println("HashCode " +  a.hashCode());
+
+        System.out.println( (char)Integer.parseInt(code));
+        System.out.println( (char)Integer.parseInt(code101));
+        System.out.println( (char)Integer.parseInt(code108));
+        System.out.println( (char)Integer.parseInt(code108));
+        System.out.println( (char)Integer.parseInt(code111));
+        System.out.println( (char)Integer.parseInt(code74));
+        System.out.println( (char)Integer.parseInt(code65));
+        System.out.println( (char)Integer.parseInt(code86));
+        System.out.println( (char)Integer.parseInt(code65));
+    }
+}
