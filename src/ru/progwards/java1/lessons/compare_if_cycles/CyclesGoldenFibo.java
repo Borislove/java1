9e0e3437437b2033c8c94df6d4634512bcbf6d0e
Boris <abirme@yandex.ru>
2020-01-11 22:18:46 +0300

    test test test

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,8 +1,15 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
+
+
 public class CyclesGoldenFibo {
+    public static  Integer iter= 1;
 
+
+    public static double gold = 1.61803;  // основание золотого треугольника
+
+
+    //OK: Тест "Тест containsDigit с нулями" пройден успешно.
    public static boolean containsDigit(int number, int digit) {
-
         if (number == digit) {
             System.out.println("true");
             return true;}
@@ -11,22 +18,29 @@ public class CyclesGoldenFibo {
 
     public static int fiboNumber(int n) {
 
-        int fibo;
         int a = 1;
         int b = 0;
+        int fibo ;
 
-        for (int i = 0; i < n; i++) {
-            fibo = a + b;
+        for (int i = 0; i < n ; i++) {
+            fibo = a+b;
             a = b;
             b = fibo;
-            System.out.println("Fibo = " + fibo);
-            //continue;
-
+            System.out.println("Iteration #"+(iter)+" "+"#a = "+ a+" #b = "+b +"#FIBO = "+ fibo);   //
+            iter++;
         }
         return 0;
     }
 
     public static boolean isGoldenTriangle(int a, int b, int c){
+
+    double a1 = 1.61703d;
+    double a2 = 1.61903d;
+    boolean gold;
+
+       //if(a,b,c == gold) {
+       //    return true;
+        //}
 
        return false;
     }
@@ -35,7 +49,8 @@ public class CyclesGoldenFibo {
     public static void main(String[] args) {
         containsDigit(32,32);
         containsDigit(32,31);
-        fiboNumber(5);
-        fiboNumber(10);
+        fiboNumber(15);
+      //  fiboNumber(10);
+
     }
 }
