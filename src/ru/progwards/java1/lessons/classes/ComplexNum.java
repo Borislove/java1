a06edd863851a12ebdb8301b0db32876c4b3090e
Boris <abirme@yandex.ru>
2020-01-12 19:19:19 +0300

    My first virus...

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -22,13 +22,18 @@ public class ComplexNum {
 
     public static int i ;
 
-    int num1;
-    int num2;
+    public static int num;
+    public static int num1 ;
+    public static int num2;
+
 
     //Инициализация комплексных чисел
     public ComplexNum(int a, int b) {
         this.a = a;
         this.b = b;
+
+       /* this.a = num1;
+        this.b = num2;*/
     }
 
     // приведение к строке, выдать в формате a+bi, например, при a=1 и b=56 должно быть выдано 1+56i
@@ -40,7 +45,13 @@ public class ComplexNum {
   //          (a + bi) + (c + di) = (a + c) + (b + d)i
     public ComplexNum add(ComplexNum num) {
 
-        return num.add(num);
+        //return new ComplexNum(num.add(num1) +(num.add(num2)  );
+        //return new ComplexNum((num.add(num)).add((num.add(num))))  ;
+
+        //return new ComplexNum( num2.add(num1),num2.add(num1));
+
+        //return num.add(num);
+        return num.add(num).add(num.add(num));
      /*   num1 = num;
         num2 = num;
         */
@@ -48,44 +59,36 @@ public class ComplexNum {
 
       //        System.out.println(z);
 
-
+        //return null;
     }
 
     // вычитание комплексных чисел по формуле:
     //            (a + bi) - (c + di) = (a - c) + (b - d)i
     public ComplexNum sub(ComplexNum num){
-        return num.sub(num);
+        //return num.sub(num);
+        return null;
     }
 
 // умножение комплексных чисел по формуле:
 //            (a + bi) * (c + di) = (a*c - b*d) + (b*c + a*d)i
     public ComplexNum mul(ComplexNum num){
-        return num.mul(num);
+        //return num.mul(num);
+        return null;
     }
 
 // деление комплексных чисел по формуле:
 //            (a + bi) / (c + di) = (a*c + b*d)/(c*c+d*d) + ((b*c - a*d)/(c*c+d*d))i
     public ComplexNum div(ComplexNum num){
-        return num.div(num);
+        //return new ComplexNum(num.num1,num.num2+num.num2);  //test
+        return null;
     }
 
     public static void main(String[] args) {
-
-
-
-
         //Ожидалось: 100+100i
         ComplexNum num1 = new ComplexNum(1,1);
         ComplexNum num2 = new ComplexNum(99,99);
-
-
-
-
          // add(ComplexNum num1 , ComplexNum num2)
-
-
         ////  ERROR: Тест "Метод add(ComplexNum num1, ComplexNum num2)" не пройден.
-        //
         //  Метод add(ComplexNum num) возвращает экземпляр класса с неверным значением toString(). Переданы параметры: num1 = new ComplexNum(1, 1), num2 = new ComplexNum(99, 99). Возвращено: 0+0i. Ожидалось: 100+100i
        // ERROR: Тест "Метод sub(ComplexNum num1, ComplexNum num2)" не пройден. Метод sub(ComplexNum num) возвращает экземпляр класса с неверным значением toString(). Переданы параметры: num1 = new ComplexNum(99, 99), num2 = new ComplexNum(1, 1). Возвращено: 0+0i. Ожидалось: 98+98i
         //ERROR: Тест "Метод mul(ComplexNum num1, ComplexNum num2)" не пройден. Метод mul(ComplexNum num) возвращает экземпляр класса с неверным значением toString(). Переданы параметры: num1 = new ComplexNum(1, 1), num2 = new ComplexNum(99, 99). Возвращено: 0+0i. Ожидалось: 0+198i
