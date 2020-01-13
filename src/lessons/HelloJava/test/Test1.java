293936d280118f5ebb25ee3096185f2e2a88e9ad
Boris <abirme@yandex.ru>
2020-01-13 10:59:39 +0300

    все умножаю

--- src/lessons/HelloJava/test/Test1.java
+++ src/lessons/HelloJava/test/Test1.java
@@ -1,19 +1,30 @@
 package lessons.HelloJava.test;
 
+import ru.progwards.java1.lessons.abstractnum.IntNumber;
+
 //
 public class Test1 {
-    public static int a;
-    public static int b;
+   private int a;
+   private int b;
 
 
     //конструктор
-  public static void Test1(int a, int b){
+  public  Test1(int a, int b){
+
       int result = a*b;
-      System.out.println(result);
+      System.out.println("Test 1# "+result);
+
+      Integer a1 = new Integer(a);
+      Integer b1 = new Integer(b);
+      Integer resultInt = a1*b1;
+      System.out.println("Test 2# "+resultInt);
+
+
+
     }
 
     public static void main(String[] args) {
-         Test1(2,3);
+      new  Test1(2,3);
 
     }
 }
