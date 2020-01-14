f3ab3322254765147fb83f4706e1f4f3fbfc526c
Boris <abirme@yandex.ru>
2020-01-14 03:50:29 +0300

    ok

--- src/ru/progwards/java1/lessons/abstractnum/Cube.java
+++ src/ru/progwards/java1/lessons/abstractnum/Cube.java
@@ -1,4 +1,10 @@
 package ru.progwards.java1.lessons.abstractnum;
 
 public class Cube {
+    public Number volume(){
+        return null;
+    }
 }
+
+/*5 Реализовать класс Cube, переопределяющий метод
+public Number volume(), который будет возвращать объем куба по формуле Segment*Segment*Segment;*/
--- src/ru/progwards/java1/lessons/abstractnum/IntNumber.java
+++ src/ru/progwards/java1/lessons/abstractnum/IntNumber.java
@@ -1,4 +1,34 @@
 package ru.progwards.java1.lessons.abstractnum;
 
 public class IntNumber {
+    public IntNumber(int num){
+
+    }
+    //@Override
+    public Number mul(Number num){
+      return null;
+    }
+    //@Override
+    public Number div(Number num){
+        return null;
+    }
+    //@Override
+    public Number newNumber(String strNum){
+        return null;
+    }
+
 }
+
+
+
+/*2. Реализовать класс IntNumber, который содержит
+2.1 конструктор
+public IntNumber(int num), который будет сохранять в классе значение целого числа.
+2.2 переопределение метода
+
+public Number mul(Number num), который будет возвращать произведение содержимого класса на num.
+2.3 переопределение метода
+public Number div(Number num), который будет возвращать частное от деления содержимого класса на num.
+2.4 переопределение метода
+public Number newNumber(String strNum), который будет возвращать новый экземпляр класса IntNumber со значением параметра strNum, содержащим целое число,, например 4 или 3. Надо будет правильно распарсить строку и привести значение к целому типу.
+2.5 public String toString() - привести значение числа к строке*/
