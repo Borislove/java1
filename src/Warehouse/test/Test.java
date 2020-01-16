3d15eeac6cb87b863c9a8938ff62f0083d8e3649
Boris <abirme@yandex.ru>
2020-01-16 20:15:21 +0300

    ...

--- src/Warehouse/test/Test.java
+++ src/Warehouse/test/Test.java
@@ -6,17 +6,27 @@ public class Test {
 
         System.out.println((n1)+(0)+(n2));
 
-        //return (n1) + + (n2);  //что это?
-      //  return (n1)+ +(n2);  //а так можно
-        //return (n1) ++ (n2);  //а так нельзя
+     //  return (n1) + + (n2);  // так можно
+    //  return (n1)+ +(n2);  // так можно
+       //return (n1) ++ (n2);  //а так нельзя
 
 
 
-        System.out.println(String.valueOf(n1)+String.valueOf(n2));
+     //   System.out.println(String.valueOf(n1)+String.valueOf(n2));
 
                 //return (String.valueOf(n1)+String.valueOf(n2));
 
-        return 0;
+     return 0;
+
+     //   Character a = ' ';
+        //hash для пробела 32
+       // System.out.println("HashCode " +  a.hashCode());
+
+      //  System.out.println(n1+a.hashCode(32)+n2);
+
+
+
+      // return (n1+a.hashCode()+n2);
 //return ((n1)+(%20)+(n2));
     }
 
