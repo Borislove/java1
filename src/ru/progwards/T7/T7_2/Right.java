f01243c9e17348b95a5c2ca4f48a7b0bc26114c7
Boris <abirme@yandex.ru>
2020-01-16 17:30:49 +0300

    все теория да теория

--- src/ru/progwards/T7/T7_2/Right.java
+++ src/ru/progwards/T7/T7_2/Right.java
@@ -6,17 +6,22 @@ public class Right {
 
     //Битовые операции
     // сдвиг вправо >>
+    // свдиг вправо с заполнениями нулями
+    //сдвиг влево <<  , аналогичен умножению на 2, на 2 свдиг на 4, на 3 умножению на 9 и т.д
+    // ~ NOT = битовое отрицание
 
     public static void main(String[] args) {
         int x = 0b1010101_01010101_01010101_01010101;
 
-        int result = x >> 1;
+        //int result = x >> 1;
+        //int result = x >>> 1;
+       /* int result = ~x;
         System.out.print("Переменная         x = ");
         System.out.println(Integer.toBinaryString(x));
        // System.out.print("Переменная    result = ");
-        System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
-        result++;
-        System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
+        System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));*/
+
+
 
       /*  while (true) {
 
@@ -28,6 +33,15 @@ public class Right {
             result++;
 
         }*/
+
+            int result = ~x;
+        System.out.print("Переменная         x = ");
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0'));
+       System.out.print("Переменная    result = ");
+        System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
+
+
     }
 }
 
