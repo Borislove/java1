27e902f290093b8a3b7253badd4990a17b5811e6
Boris <abirme@yandex.ru>
2020-01-16 17:44:10 +0300

    Битовые операции на практике: & (AND)

--- src/ru/progwards/T7/T7_2/Right.java
+++ src/ru/progwards/T7/T7_2/Right.java
@@ -1,17 +1,30 @@
 package ru.progwards.T7.T7_2;
 
-import java.sql.SQLOutput;
+// TODO: 16.01.2020 Надо будет переделать.. 
 
 public class Right {
 
     //Битовые операции
-    // сдвиг вправо >>
+    //
     // свдиг вправо с заполнениями нулями
     //сдвиг влево <<  , аналогичен умножению на 2, на 2 свдиг на 4, на 3 умножению на 9 и т.д
     // ~ NOT = битовое отрицание
 
     public static void main(String[] args) {
+
+
+        //---------------------------------------------------------------------
+        // сдвиг вправо >>
+        //---------------------------------------------------------------------
         int x = 0b1010101_01010101_01010101_01010101;
+        int result = x >> 1;
+        System.out.print("Переменная         x = ");
+        System.out.println(Integer.toBinaryString(x));
+        System.out.print("Переменная    result = ");
+        System.out.println(String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0'));
+
+
+
 
         //int result = x >> 1;
         //int result = x >>> 1;
@@ -23,25 +36,19 @@ public class Right {
 
 
 
-      /*  while (true) {
 
-          *//*  System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
-            result++;
-            System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
-*//*
-            System.out.println(Integer.toBinaryString(result));
-            result++;
-
-        }*/
-
-            int result = ~x;
+        //---------------------------------------------------------------------
+        // ~ NOT = битовое отрицание
+        //---------------------------------------------------------------------
+     /*   int x = 0b1010101_01010101_01010101_01010101;
+        int result = ~x;
         System.out.print("Переменная         x = ");
         System.out.println(Integer.toBinaryString(x));
         System.out.println(String.format("%32s", Integer.toBinaryString(x)).replace(' ', '0'));
-       System.out.print("Переменная    result = ");
+        System.out.print("Переменная    result = ");
         System.out.println(String.format("%32s", Integer.toBinaryString(result)).replace(' ', '0'));
 
-
+*/
     }
 }
 
