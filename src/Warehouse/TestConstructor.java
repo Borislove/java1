fb837f79dea1d6d83f67fe3311c0461fbf864b32
Boris <abirme@yandex.ru>
2020-01-16 16:27:14 +0300

    конструктор...

---
+++ src/Warehouse/TestConstructor.java
@@ -0,0 +1,14 @@
+package Warehouse;
+
+// TODO: 16.01.2020 Alt+Insert - вызывает конструктор 
+public class TestConstructor {
+    public TestConstructor(int a) {
+        this.a = a;
+    }
+
+    int a;
+
+    public static void main(String[] args) {
+        
+    }
+}
