097c42d2e5c4e935695184c080c6be0e67f83b7e
Boris <abirme@yandex.ru>
2020-01-17 07:54:26 +0300

    N7.3 Практика, конструкция switch case

---
+++ src/ru/progwards/T7/T7_3/SwitchCase1.java
@@ -0,0 +1,28 @@
+package ru.progwards.T7.T7_3;
+
+//Пример с оператором break
+public class SwitchCase1 {
+    public static void main(String[] args) {
+        int value = 3;
+
+        switch (value) {
+            case 1:
+                System.out.println("Выбран case 1");
+                break;
+            case 2:
+                System.out.println("Выбран case 2");
+                break;
+            case 3:
+                System.out.println("Выбран case 3");
+                break;
+            case 4:
+                System.out.println("Выбран case 4");
+                break;
+            case 5:
+                System.out.println("Выбран case 5");
+                break;
+            default:
+                System.out.println("Выбран default");
+        }
+    }
+}
