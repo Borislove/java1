ab169c396489e9bfd2c5939c0627d65a237c7caa
Boris <abirme@yandex.ru>
2020-01-17 07:37:52 +0300

    N7.3 Практика, конструкция switch case

--- src/ru/progwards/T7/T7_3/NumberSystem.java
+++ src/ru/progwards/T7/T7_3/NumberSystem.java
@@ -4,28 +4,41 @@ package ru.progwards.T7.T7_3;
 public class NumberSystem {
     //получение цифры в строков представлении по числу от 0 до 15
     public static String val2Digit(int val) {
-        return null;
+        //return null;
+        if (val == 10) {
+            return "A";
+        } else if (val == 11) {
+            return "B";
+        } else if (val == 12) {
+            return "C";
+        } else if (val == 13) {
+            return "D";
+        } else if (val == 14) {
+            return "E";
+        } else if (val == 15) {
+            return "F";
+        }
+        return Integer.toString(val);
     }
-
 
     //преобразование числа val к виду в системе счисления с основанием base
     public static String show(int val, int base) {   //функция show - показать
         String result = "";
-        while (val>0) {
+        while (val > 0) {
             int remainder = val % base;
             result = val2Digit(remainder) + result;
+            val /=base;
         }
-        if(result.isEmpty())
+        if (result.isEmpty())
             return "0";
         //return null;
         return result;
-
     }
 
     public static void main(String[] args) {
         int value = 25;
         System.out.println("Десятичная система: " + value);
-        System.out.println("Двоичная система: " + NumberSystem.show(value,2));
-        System.out.println("Шестнадцатиричная система: " + NumberSystem.show(value,16));
+        System.out.println("Двоичная система: " + NumberSystem.show(value, 2));
+        System.out.println("Шестнадцатиричная система: " + NumberSystem.show(value, 16));
     }
 }
