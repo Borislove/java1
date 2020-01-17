767165d25a88b5d6cdfc3bb681a751067a6e1f4b
Boris <abirme@yandex.ru>
2020-01-17 07:24:36 +0300

    N7.3 Практика, конструкция switch case

---
+++ src/ru/progwards/T7/T7_3/NumberSystem.java
@@ -0,0 +1,22 @@
+package ru.progwards.T7.T7_3;
+
+//представление числа в произвольной системе счисления с базой от 2 до 16включительно
+public class NumberSystem {
+    //получение цифры в строков представлении по числу от 0 до 15
+    public static String val2Digit(int val) {
+        return null;
+    }
+
+
+    //преобразование числа val к виду в системе счисления с основанием base
+    public static String show(int val, int base) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int value = 25;
+        System.out.println("Десятичная система: " + value);
+        System.out.println("Двоичная система: " + NumberSystem.show(value,2));
+        System.out.println("Шестнадцатиричная система: " + NumberSystem.show(value,16));
+    }
+}
