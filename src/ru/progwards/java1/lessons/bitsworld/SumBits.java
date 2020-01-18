b98dd00b188e9f4faa2f615d1cfd9338eda726a1
Boris <abirme@yandex.ru>
2020-01-18 14:02:30 +0300

    H6

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,11 +2,46 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static int sumBits(byte value){
-        return 1;
+
+        //Byte c = Byte.valueOf(value);
+        //System.out.println(c.hashCode());
+        //System.out.println(c.byteValue());
+
+       // c.hashCode();
+     //   System.out.println(c.hashCode(value));
+        //return c;
+        //return 1;
+/*
+        Integer test = new Integer(value);
+        System.out.println(test);
+        String test1 = test.toString();
+        System.out.println("text "+test1);
+        System.out.println(test1.hashCode());
+        System.out.println(test1.getBytes());
+        System.out.println(String.valueOf(test1).getBytes());*/
+
+
+
+        //return 0;
+
+        int result = 0;
+        for (int i = 0; i < 8; i++) {
+            if ((value & 1) == 1) {
+                result ++;
+            }
+            value >>= 1;
+        }
+
+        return result;
     }
     public static void main(String[] args) {
-        System.out.println(Byte.MAX_VALUE);
-        System.out.println(Byte.MIN_VALUE);
+      //  System.out.println(Byte.MAX_VALUE);
+      //  System.out.println(Byte.MIN_VALUE);
+
+        //sumBits((byte)0b10101);
+        System.out.println(sumBits((byte)0b101011111)) ;
+        sumBits((byte)0b0101011);
+
 
     }
 }
