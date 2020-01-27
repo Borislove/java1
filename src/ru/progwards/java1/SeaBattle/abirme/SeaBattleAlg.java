d582a743f985ea6e5156af5e809990afba71a31c
Boris <abirme@yandex.ru>
2020-01-27 23:58:52 +0300

    :octocat:  SeaBattle 3.0 , немного наглядности

--- src/ru/progwards/java1/SeaBattle/abirme/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/abirme/SeaBattleAlg.java
@@ -3,6 +3,7 @@ package ru.progwards.java1.SeaBattle.abirme;
 import ru.progwards.java1.SeaBattle.SeaBattle;
 import ru.progwards.java1.SeaBattle.SeaBattle.FireResult;
 
+
 public class SeaBattleAlg {
     // Тестовое поле создаётся конструктором
     //     SeaBattle seaBattle = new SeaBattle(true);
@@ -31,6 +32,7 @@ public class SeaBattleAlg {
     //         9|X|.|.|.|X|.|.|.|.|.|
 
 
+    public static int number = 1;
 
     //--------------------------------------Рабочий---------------------------------------- //104.678
      /*   public void battleAlgorithm(SeaBattle seaBattle) {
@@ -50,7 +52,7 @@ public class SeaBattleAlg {
 
         }*/
     //--------------------------------------------Тоже рабочий-------------------------------  // 104.587 /104.803  // всего 20 хитов, более наглядно
-    public void battleAlgorithm(SeaBattle seaBattle) {
+   /* public void battleAlgorithm(SeaBattle seaBattle) {
     int hits = 20;
         for (int y = 0; y < seaBattle.getSizeX(); y++) {
             for (int x = 0; x < seaBattle.getSizeY(); x++) {
@@ -61,23 +63,65 @@ public class SeaBattleAlg {
                     return;
             }
         }
-    }
+    }*/
 
     //-----------------------------------------------------------------------------
+
+    public void battleAlgorithm(SeaBattle seaBattle) {
+        int hits = 20;
+        //for (int x=0,y=0 ;  y < seaBattle.getSizeX() || x < seaBattle.getSizeY() ; y++ , x++) { // по диагонали
+        for (int y = 0; y < seaBattle.getSizeX(); y++) {
+            for (int x = 0; x < seaBattle.getSizeY(); x++) {
+                SeaBattle.FireResult fireResult = seaBattle.fire(x, y);
+
+                if (fireResult == FireResult.HIT) {
+                    //seaBattle.fire(x,y);
+                    //  seaBattle.getSizeY();
+                    // seaBattle.getSizeX();
+                    //FireResult.HIT;
+                    System.out.println("\u001B[31m" + "Попадание! " + "\u001B[0m" + "Победа близка");
+                    System.out.println("Координаты x = " + x + " Координаты y = " + y);
+                    System.out.println("-----------------------------------");
+                }
+
+                if (fireResult == FireResult.DESTROYED) {
+                }
+
+                if (fireResult != FireResult.MISS)
+                    hits--;
+                System.out.print("Промах  ");
+                System.out.println("Координаты x = " + x + " Координаты y = " + y);
+                System.out.println("-----------------------------------");
+                if (hits == 0)      //20 кораблей?
+                    //  System.out.println("Кораблей больше нет! Остались утки");
+                    return;
+            }
+
+        }
+
+    }
+
 
     static void testFull() {
         System.out.println("Sea battle");
         double result = 0;
-        for (int i = 0; i < 1000; i++) {  //1000
-           //    SeaBattle seaBattle = new SeaBattle(true); //игровое поле, создается тестовое поле, false или пустое новая раскладка кораблей
 
-            // TODO: 27.01.2020 для проверки
+        //---------------------Test-------------------------
+        // TODO: 27.01.2020  будет 999 итераций? 
+        for (int i = 0; i < 1000; i++) {  //1000  
+
+            System.out.println("ORIGINAL Количество попыток: " + number);
+
             SeaBattle seaBattle = new SeaBattle();
+            //   SeaBattle seaBattle = new SeaBattle(true); //игровое поле, создается тестовое поле, false или пустое новая раскладка кораблей
+
             new SeaBattleAlg().battleAlgorithm(seaBattle);
-            System.out.println("ORIGINAL");
+
             System.out.println(seaBattle); //раскладка кораблей
             result += seaBattle.getResult();
+            number++;
         }
+
         System.out.println(result / 1000);
 
     }
