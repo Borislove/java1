ebf800570dd293682a5becb82cfa7b8151870fc3
Boris <abirme@yandex.ru>
2020-01-30 00:41:43 +0300

    :octocat:  больше маленьких шагов! еще больше!

---
+++ src/Warehouse/Utils/Dated.java
@@ -0,0 +1,21 @@
+package Warehouse.Utils;
+
+
+import java.sql.Time;
+import java.util.Date;
+
+//нужна была дата, вручную вбивать влом пришлось проверить, гдето была опция по дате . а вон вижу в todo дату ставит
+
+
+
+
+public class Dated {
+    public static void main(String[] args) {
+    Date d = new Date();
+        System.out.println(d);
+
+        // TODO: 30.01.2020 что это такое?
+        Time t = new Time(1,2,3);
+        System.out.println(t);
+    }
+}
