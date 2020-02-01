6e5a2bbba14929811cbbb1a77ac4ee32ea32db08
Boris <abirme@yandex.ru>
2020-02-01 21:27:07 +0300

    :octocat: ...

---
+++ src/Warehouse/speed_racing_2020/Car_Speed.java
@@ -0,0 +1,82 @@
+package Warehouse.speed_racing_2020;
+
+
+// TODO: 01.02.2020 Эксперименты и развлечение...
+public class Car_Speed {
+
+
+    private static int speed = 0;
+
+    private static final int MAX_SPEED = 60;
+
+    private static void speedUp() {
+        speed++;
+    }
+
+    private static void speedDown() {
+        speed--;
+    }
+
+    private static void speedAcceleration() {   //ускорение
+        speed += 10;
+    }
+
+    private static void speedStop() {
+        speed = 0;
+    }
+
+    public static void main(String[] args) {
+
+
+        if (speed <= MAX_SPEED) {
+
+            System.out.println("Текущая скорость: " + speed);
+
+            System.out.println("Прибавили: ");
+            speedUp();
+            System.out.println("Текущая скорость: " + speed);
+
+            System.out.println("Убавили: ");
+            speedDown();
+            System.out.println("Текущая скорость: " + speed);
+
+            System.out.println("Ускорение: ");
+            speedAcceleration();
+            System.out.println("Текущая скорость: " + speed);
+
+/*
+            for (int i = 0; i <= MAX_SPEED; i++) {
+                speedAcceleration();
+                System.out.println("Ускорение ");
+                if(i==MAX_SPEED) {
+                    System.out.println("Двигатель сгорел");
+                }*/
+
+            while (true) {
+                int ver = new java.util.Scanner(System.in).nextInt();
+                int a = Integer.valueOf(ver);
+                switch (a) {
+                    case 1:
+                        speedUp();
+                        System.out.println("Текущая скорость: " + speed);
+                        break;
+                    case 2:
+                        speedDown();
+                        System.out.println("Текущая скорость: " + speed);
+                        break;
+                    case 3:
+                        speedAcceleration();
+                        System.out.println("Текущая скорость: " + speed);
+                        break;
+                    case 4:
+                        speedStop();                //speed 0 , когда понижение назад будет отсчет
+                        System.out.println("Текущая скорость: " + speed);
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
+
---
+++ src/ru/progwards/t4/n4_1/Example.java
@@ -0,0 +1,13 @@
+package ru.progwards.t4.n4_1;
+
+public class Example {
+    public static void main(String[] args) {
+
+        int result = 1;
+        System.out.print("Сумма равна: ");
+        System.out.println(result);
+
+        //---------------------------------------
+        System.out.println("Сумма равна: "+ result);
+    }
+}
