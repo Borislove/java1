adade1c5e5e355cae0cacf2b9bc09837eef4a04b
Boris <abirme@yandex.ru>
2020-02-06 16:15:57 +0300

    :octocat: My horse

--- src/Warehouse/horse/Horse.java
+++ src/Warehouse/horse/Horse.java
@@ -1,13 +1,16 @@
 package Warehouse.horse;
 
 //эксперименты...
+//со временем беда, надо математику учить
+//лошадь бежит и уже хорошо
 public class Horse {
 
-    private static String name = "Розовый пони";
+    private static String name = "\u001B[35m"+"Розовый пони"+"\u001B[0m";
     private static int speed = 0;
     private static int distance = 100;
 
-    private static int time ;
+    //private static int time = 1000;
+    private static int time;
 
     private static void speedUp() {
         speed++;
@@ -18,9 +21,11 @@ public class Horse {
         distance--;
     }
 
-    private static void time(){
-        time = 1000; //1000 секунд
-        time = distance/speed; //и в чем время измеряется то?
+    private static void time() {
+        // time = 1000; //1000 секунд
+        //и в чем время измеряется то?
+        //time--;
+        time = distance / speed;
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -37,7 +42,7 @@ public class Horse {
             speedUp();
             distance();
             time();
-            System.out.print("время "+time+" расстояние " + distance + " скорость:" + speed + "\r");
+            System.out.print("время " + time + " расстояние " + distance + " скорость:" + speed + "\r");
 
             //через свитч попробовать?
             if (speed == 30) {
@@ -45,10 +50,30 @@ public class Horse {
                 System.out.println(name);
                 continue;
             }
-            if (speed == 50) {  //не пашет
+            if (speed >= 50) {  //не пашет
                 name = "\u001B[36m" + name;
+            }
+            int field = distance;
+            switch (field) {
+                case 90:
+                    System.out.println("\r"+"бежит через поле");
+                    break;
+
+                case 80:
+                    System.out.println("бежит через лес");
+                    break;
+
+                case 70: //не сработал
+                    System.out.println("скачет...");
+                    break;
+            }
+
+            if(distance==0) {
+                System.out.println("Лошадка прибыла в загон!");
+                break;
             }
         }
     }
 }
+
 
