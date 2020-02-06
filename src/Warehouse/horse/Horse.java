316a3468a930fb9e9e652f0cc407720304754181
Boris <abirme@yandex.ru>
2020-02-06 16:37:39 +0300

    :octocat: My horse

--- src/Warehouse/horse/Horse.java
+++ src/Warehouse/horse/Horse.java
@@ -3,14 +3,30 @@ package Warehouse.horse;
 //эксперименты...
 //со временем беда, надо математику учить
 //лошадь бежит и уже хорошо
+//надо коллекции изучать)
+
+/**
+ * @author FreeCoder
+ * Thu Feb 06  ~ 16:36:07 MSK 2020
+ */
 public class Horse {
 
-    private static String name = "\u001B[35m"+"Розовый пони"+"\u001B[0m";
+    private static String name = "\u001B[35m" + "Розовый пони" + "\u001B[0m";
     private static int speed = 0;
     private static int distance = 100;
 
     //private static int time = 1000;
     private static int time;
+
+    private static String resource[] = {"блошка", "цветок", "ветерок", "репейник"};  //блошка к скорости, репейник к тормозам XD
+    private static String res = "";
+
+    private static void resource() {
+        for (String value : resource) {
+            res = value;
+            //   System.out.println(res);
+        }
+    }
 
     private static void speedUp() {
         speed++;
@@ -39,6 +55,7 @@ public class Horse {
 
         while (true) {
             Thread.sleep(500);
+            resource();
             speedUp();
             distance();
             time();
@@ -56,7 +73,7 @@ public class Horse {
             int field = distance;
             switch (field) {
                 case 90:
-                    System.out.println("\r"+"бежит через поле");
+                    System.out.println("\r" + "бежит через поле");
                     break;
 
                 case 80:
@@ -68,8 +85,9 @@ public class Horse {
                     break;
             }
 
-            if(distance==0) {
+            if (distance == 0) {
                 System.out.println("Лошадка прибыла в загон!");
+                System.out.println("собрано: " + res);
                 break;
             }
         }
