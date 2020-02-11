81e431c10e55051ddf0aff567136528b88923eb5
Boris <abirme@yandex.ru>
2020-02-11 18:51:37 +0300

    :octocat: test

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -9,7 +9,8 @@ package ru.progwards.java1.lessons.bitsworld;
 */
 public class SumBits {
 
-    public static int sumBits(byte value) {
+    //-------------------------------------------
+   /* public static int sumBits(byte value) {
 
         int result = 0;
         for (int i = 0; i < 8; i++) {
@@ -20,12 +21,28 @@ public class SumBits {
         }
 
         return result;
+    }*/
+    //---------------------------------------------
+
+    public static int sumBits(byte value) {
+        int sum = 0;
+        for (int i = 0; i < 8; i++) {
+            sum += (value >> i) & 1;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
 
         System.out.println(sumBits((byte) 0100101));
+        System.out.println(sumBits((byte) 10100101)); //3
         //  System.out.println(sumBits((byte) 0111100)); //4
     }
 }
 
+
+
+/*Задача 1. Класс SumBits: пройдено, оценка: 16.0
+Комментарий:
+OK: Тест "Функция sumBits(byte value)" пройден успешно.
+По данной задаче в целом зачет, решение принято. Задача выполнена на 100.00%*/
