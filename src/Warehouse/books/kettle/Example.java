3a95a3573ba359512aae3300a3a0b87a2379cd8a
Boris <abirme@yandex.ru>
2020-02-24 04:47:24 +0300

    :octocat: learning...

---
+++ src/Warehouse/books/kettle/Example.java
@@ -0,0 +1,31 @@
+package Warehouse.books.kettle;
+
+//Эксперименты с комментами, /* думал перектроет предыдущий коммент , но увы надо закрывать
+// что то типа этого        /* текст
+                                /* текст2
+
+                                        */
+public class Example {
+    public static void main(String[] args) {
+
+        System.out.println("test");
+        System.out.println("test1");
+        /*
+        * text text text
+        * *//*
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");*/
+
+
+
+/*        System.out.println("test");
+        System.out.println("test1");
+        /*
+         * text text text
+         * *//*
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");*/
+    }
+}
--- src/Warehouse/utils/variables/Main.java
+++ src/Warehouse/utils/variables/Main.java
@@ -1,41 +1,55 @@
 package Warehouse.utils.variables;
 
+
+import java.util.Scanner;
+
 public class Main {
 
-    private static int verse = 1;
+    // private static int verse = 1;
 
-    public static void main(String[] args) {
-
+    public static void variables() {
+        System.out.println("----------------------------");
         System.out.println("Float -  1");
         System.out.println("Long  - 2");
         System.out.println("Double  - 3");
         System.out.println("Integer - 4");
         System.out.println("Short - 5");
+        System.out.println("----------------------------");
+    }
 
-        //Float.Float();
-        //Long.Long();
-        //Double.Double();
-        // Integer.Integer();
-        // Short.Short();
-        switch (verse) {
-            case 1:
-                Float.Float();
-                break;
-            case 2:
-                Long.Long();
-                break;
-            case 3:
-                Double.Double();
-                break;
-            case 4:
-                Integer.Integer();
-                break;
-            case 5:
-                Short.Short();
-                break;
+    public static void main(String[] args) {
 
-            default:
-                System.out.println("error");
+        variables();
+        while (true) {
+            Scanner key = new Scanner(System.in);
+            int verse = key.nextInt();
+
+            //Float.Float();
+            //Long.Long();
+            //Double.Double();
+            // Integer.Integer();
+            // Short.Short();
+            switch (verse) {
+                case 1:
+                    Float.Float();
+                    break;
+                case 2:
+                    Long.Long();
+                    break;
+                case 3:
+                    Double.Double();
+                    break;
+                case 4:
+                    Integer.Integer();
+                    break;
+                case 5:
+                    Short.Short();
+                    break;
+                default:
+                    System.out.println("error");
+                    break;
+            }
+            variables();
         }
     }
 }
