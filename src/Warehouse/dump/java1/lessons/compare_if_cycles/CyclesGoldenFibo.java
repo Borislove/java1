4d33e56b64edf49c33f4bd3d9f9528521fcdeadf
Boris <abirme@yandex.ru>
2020-03-16 06:18:55 +0300

    :octocat: ...test H4.3 test

--- src/Warehouse/dump/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/Warehouse/dump/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -30,36 +30,36 @@ public class CyclesGoldenFibo {
     //if (digit >= 0 && digit <= 9) {
     // System.out.println("Число = " + number + " Цифра = " + digit);
 
-//    OK: Тест "Тест fiboNumber" пройден успешно.
+    //    OK: Тест "Тест fiboNumber" пройден успешно.
     public static int fiboNumber(int n) {
 
-       if (n == 1 || n == 2)
+        if (n == 1 || n == 2)
             return 1;
 
         int a = 1;
         int b = 0;
-        int fibo = 0 ;
+        int fibo = 0;
 
-        for (int i = 0; i < n ; i++){
-            fibo = a+b;
+        for (int i = 0; i < n; i++) {
+            fibo = a + b;
             a = b;
             b = fibo;
-         //   System.out.println("Iteration #"+(iter)+" "+"#a = "+ a+" #b = "+b +"#FIBO = "+ fibo);   //
+            //   System.out.println("Iteration #"+(iter)+" "+"#a = "+ a+" #b = "+b +"#FIBO = "+ fibo);   //
             iter++;
-         }
+        }
         return fibo;
     }
 
-    public static boolean isGoldenTriangle(int a, int b, int c){
+    public static boolean isGoldenTriangle(int a, int b, int c) {
 
-    boolean gold = false ; //default false;
+        boolean gold = false; //default false;
 
-    double value1 = 1.61703;
-    double value2 = 1.61903;
+        double value1 = 1.61703;
+        double value2 = 1.61903;
 
-        double a1 = (double)a;
-        double b1 = (double)b;
-        double c1 = (double)c;
+        double a1 = (double) a;
+        double b1 = (double) b;
+        double c1 = (double) c;
 
         double ab = a1 / c1;
         double ac = a1 / b1;
@@ -71,45 +71,44 @@ public class CyclesGoldenFibo {
         double ac = a / c;
         double bc = b / c;*/
 
-        if(a==b ) {
+        if (a == b) {
             if (ab > value1 && ab < value2) {
-                System.out.println("Золотой треугольник!" + a + " "+b+" "+c);
+                System.out.println("Золотой треугольник!" + a + " " + b + " " + c);
                 gold = true;
-                }
+            }
         }
 
-            if(a==c ) {
-                if (ac > value1 && ac < value2) {
-                    System.out.println("Золотой треугольник!" + a + " "+b+" "+c);
-                    gold = true;
-                }
+        if (a == c) {
+            if (ac > value1 && ac < value2) {
+                System.out.println("Золотой треугольник!" + a + " " + b + " " + c);
+                gold = true;
+            }
         }
 
-        if(b==c ) {
+        if (b == c) {
             if (bc > value1 && bc < value2) {
-                System.out.println("Золотой треугольник!" + a + " "+b+" "+c);
+                System.out.println("Золотой треугольник!" + a + " " + b + " " + c);
                 gold = true;
             }
         }
 
 
-       return gold;
-   }
+        return gold;
+    }
 
     public static void main(String[] args) {
-       containsDigit(2,0);
-       containsDigit(3,-2);
-       containsDigit(-3,10);
-       containsDigit(23123,9);
-      //  containsDigit(32,32);
-      //  containsDigit(32,31);
+        containsDigit(2, 0);
+        containsDigit(3, -2);
+        containsDigit(-3, 10);
+        containsDigit(23123, 9);
+        //  containsDigit(32,32);
+        //  containsDigit(32,31);
         fiboNumber(15);
-      //  fiboNumber(10);
+        //  fiboNumber(10);
 
 
-        System.out.println(isGoldenTriangle(23,23,59));
+        System.out.println(isGoldenTriangle(23, 23, 59));
         System.out.println(isGoldenTriangle(89, 55, 89)); // золотой
-
 
 
         //-------------------------------------
@@ -120,13 +119,15 @@ public class CyclesGoldenFibo {
            }*/
 
         for (int i = 1; i <= 100; i++) {
-            for (int j = 1; j <= 100 ; j++) {
-                for (int k = 1; k <= 100 ; k++) {
+            for (int j = 1; j <= 100; j++) {
+                for (int k = 1; k <= 100; k++) {
                     if (TriangleInfo.isTriangle(i, j, k) && TriangleInfo.isIsoscelesTriangle(i, j, k) && isGoldenTriangle(i, j, k)) {
                         System.out.println(i + " " + j + " " + k);
                     }
 
+                }
+            }
         }
- }}
- }}
+    }
+}
 
