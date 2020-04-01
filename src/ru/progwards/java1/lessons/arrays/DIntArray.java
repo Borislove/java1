3046914d31e912faa60e8e362ff32c54294202a5
Boris <abirme@yandex.ru>
2020-04-02 02:08:36 +0300

    :octocat: ...test H6 test

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -85,12 +85,6 @@ public class DIntArray {
 
     private int[] nums = {};
 
-    DIntArray() {
-    }
-
-    ;
-
-    // добавляет элемент num в конец массива
     public void add(int num) {
         int[] nums1 = new int[nums.length + 1];
         System.arraycopy(nums, 0, nums1, 0, nums.length);
@@ -98,24 +92,19 @@ public class DIntArray {
         nums = nums1;
     }
 
-    // добавляет элемент num в позицию pos массива
     public void atInsert(int pos, int num) {
         int[] nums1 = new int[nums.length + 1];
-        System.arraycopy(nums, 0, nums1, 0, pos);
         System.arraycopy(nums, pos, nums1, pos + 1, nums.length - pos);
         nums1[pos] = num;
         nums = nums1;
     }
 
-    // удаляет элемент в позиции pos массива
     public void atDelete(int pos) {
         int[] nums1 = new int[nums.length - 1];
-        System.arraycopy(nums, 0, nums1, 0, pos);
         System.arraycopy(nums, pos + 1, nums1, pos, nums.length - pos - 1);
         nums = nums1;
     }
 
-    // возвращает элемент по индексу pos
     public int at(int pos) {
         return nums[pos];
     }
