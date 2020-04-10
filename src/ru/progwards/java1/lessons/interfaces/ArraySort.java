838b70e839e2e5077d42d5cdd68efcd523d0344a
Boris <abirme@yandex.ru>
2020-04-10 11:32:29 +0300

    :octocat: ...test

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -1,4 +1,19 @@
 package ru.progwards.java1.lessons.interfaces;
 
 public class ArraySort {
+
+    public static void sort(int[] a) {
+        int number = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    number = a[i];
+                    a[i] = a[j];
+                    a[j] = number;
+                }
+            }
+        }
+    }
 }
+
+
