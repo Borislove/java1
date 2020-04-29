517af1bab58d4941314bf582a8fa464575245df2
Borislove <abirme@yandex.ru>
2020-04-29 03:57:52 +0300

    8 test test

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -4,24 +4,56 @@ package ru.progwards.java1.lessons.interfaces;
 // класса ArraySort из задачи 2 урока 6 и переделать сортировку массива
 // целых чисел на сортировку через интерфейс CompareWeight public static void sort(CompareWeight[] a)
 
-public class ArraySort implements Comparable<CompareWeight> {
-    @Override
-    public int compareTo(CompareWeight o) {
-        return 0;
-    }
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-    public static void sort(int[] a) {
-        int number = 0;
+public abstract class ArraySort implements Comparable<CompareWeight> {
+
+    /*static CompareWeight compareWeight = {CompareWeight.CompareResult.LESS, CompareWeight.CompareResult.GREATER, CompareWeight.CompareResult.EQUAL};
+
+     *//*public CompareWeight[] getCompareWeight() {
+        return compareWeight;
+    }*//*
+
+    public static void sort(CompareWeight[] a) {
+
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    number = a[i];
+                if (a[i].equals(a[j])) {
+                    compareWeight = a[i];
                     a[i] = a[j];
                     a[j] = number;
                 }
             }
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(compareWeight));
+    }
+}*/
+
+    /*
+
+        public static void sort(int[] a) {
+            int number = 0;
+            for (int i = 0; i < a.length; i++) {
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[i] > a[j]) {
+                        number = a[i];
+                        a[i] = a[j];
+                        a[j] = number;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    */
+
+    public static void sort(CompareWeight[] a) {
+
+    }
 }
-
-
--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -2,7 +2,13 @@ package ru.progwards.java1.lessons.interfaces;
 
 //3.1 Создать интерфейс CompareWeight
 interface CompareWeight {
+
     double weight = 0d;
+
+    /*CompareResult less = CompareResult.LESS;
+    CompareResult equal = CompareResult.EQUAL;
+    CompareResult greater = CompareResult.GREATER;
+*/
 
 
     //3.2 В нем создать public enum CompareResult, со значениями LESS, EQUAL, GREATER
@@ -10,6 +16,7 @@ interface CompareWeight {
         LESS,   //меньше
         EQUAL,   //равно
         GREATER   //повышение
+        ;
     }
 
     /* 3.3 Интерфейс должен содержать метод public CompareResult compareWeight(CompareWeight smthHasWeigt)*/
