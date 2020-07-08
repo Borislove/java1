afe40be8c85826676a7d95c13e135420d58e5893
Borislove <abirme@yandex.ru>
2020-07-08 21:25:56 +0300

    4.1 test

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -1,55 +1,36 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
+//Задача 1. Класс TriangleSimpleInfo
+
 public class TriangleInfo {
-    /*
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    2.1 Реализовать функцию
-    public static boolean isTriangle(int a, int b, int c), которая возвращает true,
-    если по данным трём сторонам (a, b, c) можно построить треугольник. Из геометрии
-        известно, что в треугольнике длина каждой из сторон меньше суммы длин двух других сторон.
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    */
-    public static boolean isTriangle(int a, int b, int c) {
 
-        if (a < b + c && b < a + c && c < a + b && a > 0 && b > 0 && c > 0) {
-            return true;
-        }
-        return false;
+    /*1.1 Реализовать функцию
+    public static int maxSide(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника, а вернуть должна наибольшую длину стороны.*/
+    public static int maxSide(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
     }
 
-    /*
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    2.2 Реализовать функцию
-    public static boolean isRightTriangle(int a, int b, int c), которая возвращает true, если треугольник
-    со сторонами a, b, c является прямоугольным. Из геометрии известно, что для прямоугольного треугольника
-     выполняется теорема Пифагора (сумма квадратов катетов равна квадрату гипотенузы).
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     */
-    public static boolean isRightTriangle(int a, int b, int c) {
-
-        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a && a > 0 && b > 0 && c > 0) {
-            return true;
-        } else
-            return false;
+    /*1.2 Реализовать функцию
+    public static int minSide(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника, а вернуть должна наименьшую длину стороны.*/
+    public static int minSide(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 
-    /*
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    2.3 Реализовать функцию
-    public static boolean isIsoscelesTriangle(int a, int b, int c), которая возвращает true,
-    если треугольник со сторонами a, b, c является равнобедренным. Из геометрии известно,
-    что в равнобедренном треугольнике есть две равные стороны.
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     */
-    public static boolean isIsoscelesTriangle(int a, int b, int c) {
+    /*1.3 Реализовать функцию
+    public static boolean isEquilateralTriangle(int a, int b, int c),
+    которая получает параметрами длины сторон треугольника, а вернуть должна true,
+    если треугольник равносторонний и false в противном случае.*/
 
-        if (a == c || b == c || c == b && a > 0 && b > 0 && c > 0) {
-            return true;
-        } else {
-            return false;
-        }
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        return a == b && b == c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSide(8, 4, 3));
+        System.out.println(maxSide(0, 4, 3));
+        System.out.println(minSide(8, 4, 3));
+        System.out.println(isEquilateralTriangle(2, 2, 1));
     }
 }
-
-
-
