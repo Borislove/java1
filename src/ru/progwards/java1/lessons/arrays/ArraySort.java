0e5f41c3dcb3d67e49555e7e94533b083c988169
Borislove <abirme@yandex.ru>
2020-07-24 16:55:13 +0300

    6,test

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -19,18 +19,19 @@ import java.util.Arrays;
 public class ArraySort {
 
     public static void sort(int[] a) {
-
-
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < i + 1; j++) {
-
+            for (int j = i+1; j <  a.length; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
     }
 
-
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+        int[] array = {1, 2, 3, 4, 5,9,0,-3};
         sort(array);
         System.out.println(Arrays.toString(array));
     }
