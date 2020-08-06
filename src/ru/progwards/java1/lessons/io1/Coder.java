0cec6fe5d422b1e9f61a4deac8db81a32bd1d017
Borislove <abirme@yandex.ru>
2020-08-06 17:03:09 +0300

    test io1  log

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -35,7 +35,8 @@ public class Coder {
                     String str = scanner.nextLine();
                     System.out.println(str);
 
-                    in.write(str);
+                    for (Character c : code)
+                        in.write(c);
                 }
 
 
@@ -45,25 +46,7 @@ public class Coder {
 
 
                 //*****************************************************************
-                while (scanner.hasNextLine()) {
 
-                    //*while (scanner.hasNext()) {
-
-                    String strFromFile = scanner.nextLine();
-
-
-                    for (int i = 0; i < strFromFile.length(); i++) {
-                        int a = 0;
-                        // in.write(strFromFile + "\n");
-                        for (char value : code) {
-                            //    System.out.println((int) value);
-                            a += value;
-//                            System.out.println("\n");
-                            // in.write("" + Integer.valueOf(a));   //строка
-                            in.write("" + Integer.valueOf(a) + "\n");   //столбец
-                        }
-                    }
-                }
                 //*****************************************************************
                 in.close();
                 scanner.close();
