7eaf1fe81e28f349a52e4be5a24efc805eb262b0
Borislove <abirme@yandex.ru>
2020-08-27 17:19:44 +0300

    collections

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -2,13 +2,16 @@ package ru.progwards.java1.lessons.collections;
 
 
 import java.util.*;
+/*
 
 public class Creator {
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*1.1 Реализовать метод
+    */
+/*1.1 Реализовать метод
     public static Collection<Integer> fillEven(int n) - создать коллекцию и заполнить
-     последовательностью четных возрастающих чисел начиная с 2, количество элементов в коллекции n*/
+     последовательностью четных возрастающих чисел начиная с 2, количество элементов в коллекции n*//*
+
     public static Collection<Integer> fillEven(int n) {
 
         List<Integer> arrayList = new ArrayList();
@@ -23,17 +26,21 @@ public class Creator {
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*1.2 Реализовать метод
+    */
+/*1.2 Реализовать метод
     public static Collection<Integer> fillOdd(int n) - создать коллекцию и заполнить
-     последовательностью нечетных убывающих чисел, минимальное число в коллекции 1, количество элементов в коллекции n*/
+     последовательностью нечетных убывающих чисел, минимальное число в коллекции 1, количество элементов в коллекции n*//*
+
     public static Collection<Integer> fillOdd(int n) {
         // List<Integer> arrayList = new ArrayList();
-       /* int num = 0;
+       */
+/* int num = 0;
         for (int i = 0; i < n; i++) {
             num += 3;
             arrayList.add(num);
         }
-        return arrayList;*/
+        return arrayList;*//*
+
         //--------------------------------------
 
         List<Integer> linkedList = new LinkedList();
@@ -55,10 +62,12 @@ public class Creator {
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*1.3 Реализовать метод
+    */
+/*1.3 Реализовать метод
     public static Collection<Integer> fill3(int n) - создать коллекцию и заполнить ее тройками чисел.
     Каждая тройка создается по алгоритму: первое число тройки - индекс числа в коллекции,
-    второе - индекс в квадрате, третье - индекс в кубе, количество элементов в коллекции n*3*/
+    второе - индекс в квадрате, третье - индекс в кубе, количество элементов в коллекции n*3*//*
+
     public static Collection<Integer> fill3(int n) {
         ArrayList<Integer> list = new ArrayList(n);
         return list;
@@ -68,18 +77,74 @@ public class Creator {
 
     public static void main(String[] args) {
 
-     /*   ArrayList list = new ArrayList();
+     */
+/*   ArrayList list = new ArrayList();
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
         System.out.println(list);
 
-        fillEven(10);*/
+        fillEven(10);*//*
+
 
         // fillEven(14);
         //  System.out.println(fillEven(14));
 
         fillOdd(15);
+    }
+}
+*/
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Creator {
+
+    public static Collection<Integer> fillEven(int n){
+        List<Integer> arrayList = new ArrayList();
+        int m = 0;
+        for (int i=0; i<n; i++){
+            m += 2;
+            arrayList.add(m);
+        }
+        return arrayList;
+    }
+
+    public static Collection<Integer> fillOdd(int n){
+        List<Integer> arrayList = new ArrayList();
+        int m = n*2+1;
+        for (int i=0; i<n; i++){
+            m -= 2;
+            arrayList.add(m);
+        }
+        return arrayList;
+    }
+
+    public static Collection<Integer> fill3(int n){
+        List<Integer> arrayList = new ArrayList();
+
+
+        for (int i=0; i<n; i++){
+
+            List<Integer> arrayList3 = new ArrayList();
+            arrayList3.add(0,i*3);
+            arrayList3.add(1,i*i*9);
+            arrayList3.add(2,i*i*i*27);
+
+            arrayList.addAll(arrayList3);
+
+        }
+        return arrayList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("четные по возрастанию");
+        System.out.println(fillEven(26));
+        System.out.println("нечетных убывающих чисел");
+        System.out.println(fillOdd(8));
+        System.out.println("тройками чисел");
+        System.out.println(fill3(5));
     }
 }
 
