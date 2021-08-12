a0daf782094a74eef396af2e9a09e47c9c30e47d
Borislove <abirme@yandex.ru>
2021-08-12 12:02:24 +0300

    test test

--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -6,6 +6,6 @@ package ru.progwards.java1.lessons.test;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+        //System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
     }
 }
