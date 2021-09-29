2412cffafa9ef67151aae165f34354a696b98b68
Borislove <abirme@yandex.ru>
2021-09-29 18:13:53 +0300

    h5 , home work

---
+++ src/ru/progwards/java1/lessons/classes/Main.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Cow(25));
+        System.out.println(new Hamster(20));
+        System.out.println(new Animal(99));
+        System.out.println(new Duck(232));
+    }
+}
